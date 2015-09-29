c1c0031cea43c7f5addc45eb1c7e6d4f146789f8
Jason de Belle <jaytheman@gmail.com>
2015-09-28 23:49:04 -0400

    Updated README and added main script to invert webpages

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# eye-invert
-Tamper Monkey script to invert colors of webpages. 
+# <img width="150px" src="https://raw.githubusercontent.com/CynderR/MarkupTests/master/images/pirate-penguin.png" alt="Pirate Penguin"> &nbsp;&nbsp; iEye!
+### ARrrr, Save your eyes Matey.
+
+Turn your searches as dark as your terminal and text editor.
+#### A TamperMonkey script to invert colors of webpages.
+Ahoy, Matey, This script can invert the colors of the page you are on, remember domains to auto invert and exclude path's from domains.
+
+#### To use :
+1. Install TamperMonkey
+2. Add iEye.js to tamperMonkey
+3. Once installed press ctrl+q to invert a page (or change keycode 81)
+4. (optional) Add domains to the "autoChange" array to have them automatically invert on load.
+5. (optional) Add exclude paths to domains that have pages that might not need to be inverted.
+
+#### Screen shots
+Avast ye and check out these screenshots!
+![alt text](https://raw.githubusercontent.com/CynderR/MarkupTests/master/screenshots/stackoverflow.png "Inverted Stackoverflow")
+
+![alt text](https://raw.githubusercontent.com/CynderR/MarkupTests/master/screenshots/google.png "Inverted google search")
+
+
+##### Note:
+This script only works with chrome because of the webkit-filter: invert(100%);
+
+
---
+++ js/iEye.js
@@ -0,0 +1,101 @@
+// ==UserScript==
+// @name         Invert Colors2 (Chrome)
+// @namespace    http://themoviehacker.com/
+// @version      0.1
+// @description  Invert any page color by pressing ctrl+q, auto invert any page by adding domain in the autoChange array
+// @author       Jason de Belle
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    // the css we are going to inject
+  var iEye = {
+
+    keycodes: [81],
+
+    autoChange: [
+      "github.com",
+      "www.google.com",
+      "www.google.ca",
+      "plus.google.com",
+      "git-scm.com",
+      "gist.github.com",
+      "www.gnu.org",
+      "8tracks.com",
+      "www.wikipedia.org",
+      "stackoverflow.com",
+      "en.wikipedia.org",
+      "underscorejs.org",
+      "backbonejs.org",
+      "developer.mozilla.org",
+      "nixsrv.com",
+      "askubuntu.com",
+      "githowto.com",
+      "www.thesaurus.com"
+    ],
+
+    exclude: {
+        "www.google.ca": ["/_/chrome/newtab"]
+    },
+    uniqueStyle: "i-eye-style",
+
+
+
+    css: " html {-webkit-filter: invert(100%);}" +
+      " body{background-color: black;} " +
+            " img {-webkit-filter: invert(100%);}" +
+            " object {-webkit-filter: invert(100%);}" +
+            " video {-webkit-filter: invert(100%);}" +
+            " png {-webkit-filter: invert(100%);}" +
+            " * {color:#663355}" +
+            " .added.modified.line {-webkit-filter: invert(100%);}" +
+            " .removed.modified.line {-webkit-filter: invert(100%);}",
+
+    host: window.location.hostname,
+    path: window.location.pathname,
+    head: document.getElementsByTagName('head')[0],
+
+
+    invertColor: function () {
+      var style = document.getElementById(this.uniqueStyle);
+      if(!style) {
+        style = document.createElement('style');
+        //injecting the css to the head
+        style.type = 'text/css';
+        style.id = this.uniqueStyle;
+        style.appendChild(document.createTextNode(this.css));
+        this.head.appendChild(style);
+      } else {
+        // Undo invert clicking the bookmarklet again
+        style.remove();
+      }
+    },
+
+    init: function() {
+      document.addEventListener('keydown', function(e) {
+        if (this.keycodes.indexOf(e.keyCode) != -1 && e.ctrlKey) {
+          e.cancelBubble = true;
+          e.stopImmediatePropagation();
+
+          this.invertColor();
+        }
+        return false;
+      }.bind(this));
+
+      // Auto load sections
+      for (var auto in this.autoChange) {
+        if (this.autoChange[auto] === this.host) {
+          // If host matchs, check path
+          for (var excludeItem in this.exclude[this.host]) {
+            if (this.path == this.exclude[this.host][excludeItem]) {
+              return;
+            }
+          }
+          this.invertColor();
+        }
+      }
+    }
+  };
+  iEye.init(this);
+}());
